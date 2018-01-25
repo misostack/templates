9b89c3db17589bf97b274587423d7b1e5a396e9e
Nguyen Minh Son <misostack.com@gmail.com>
2018-01-25 16:42:24 +0700

    Add custom palettes

--- techblog2017/src/modules/site.js
+++ techblog2017/src/modules/site.js
@@ -8,6 +8,7 @@ var Site = Site || {};
       this.navBarEvent();
       this.scrollEvent();
       this.lazyLoading();
+      this.scrollpy();
     },
     // highlight code
     highlightcode: function(){
@@ -20,6 +21,27 @@ var Site = Site || {};
       // console.log(hljs.listLanguages());
       // hljs.configure({useBR: true});
       Prism.highlightAll();
+    },
+    // scrollpy
+    scrollpy: function(){
+      if( $('.article-scrollby').length > 0 ){
+        var $article_scrollby = $('.article-scrollby');
+        var article_tiles = $article_scrollby.find(':header');
+        var $article_scrollby_sidebar = $($article_scrollby.attr('data-target'));
+        var pos_buffer = $('.navbar-fixed').outerHeight();
+        if( $article_scrollby_sidebar.length > 0 ){
+          var $article_scrollpy_links = $article_scrollby_sidebar.find('.article-scrollpy-links');
+          article_tiles.each(function(index, ele){
+            $(ele).attr('id', 'article-titles-' + index);
+            $article_scrollpy_links.append('<li><a class="article-titles-link" href="javascript:void(0);" data-href="#' + 'article-titles-' + index + '">' + $(ele).text() + '</a></li>');
+          });
+          // add click event
+          $article_scrollpy_links.on('click','.article-titles-link', function(){
+            
+            Site.smoothScrollToElement( $(this).attr('data-href'), pos_buffer * -1);
+          });          
+        }
+      }
     },
     // lazy loading
     lazyLoading: function(){
@@ -61,6 +83,24 @@ var Site = Site || {};
         // hover out
         $(this).removeClass('hover');
       });
+    },
+    // smoothScrollTo
+    smoothScrollTo: function ( pos, callback) {
+      $('html, body')
+      .animate({ scrollTop: pos }, 1000)
+      .promise()
+      .then(function(){
+          // callback code here
+          if (typeof callback === "function") {
+              callback();
+          }
+      });
+    },
+    smoothScrollToElement: function( selector, pos_buffer ) {
+      if( $(selector).length > 0 ){
+        var pos = $(selector).offset().top + pos_buffer;
+        this.smoothScrollTo(pos, null);
+      }
     }
   };
 })(jQuery);
